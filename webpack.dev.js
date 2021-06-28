31df800fa09e67ec2654762f3725eb64ce46bd86
Namig Hajiyev <namiq886@gmail.com>
2021-06-28 13:43:05 +0400

    extract css to files and minfy css

--- webpack.dev.js
+++ webpack.dev.js
@@ -21,8 +21,37 @@ module.exports = merge(common, {
             template: './src/index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {    loader: 'css-loader'},
+                    {
+                        // Run postcss actions
+                        loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: function () {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+                    },
+                    { loader: 'sass-loader' },
+                ]
+            }
+        ]
+    },
 
-    devServer: {
+    devServer:
+    {
         contentBase: [
             path.resolve(__dirname, 'build')
         ],
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require('path');
 const common = require("./webpack.common");
 const merge = require("webpack-merge").merge;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -19,9 +21,52 @@ module.exports = merge(common, {
                 collapseWhitespace: true,
                 removeComments: true
             }
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" })
     ],
-
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin({
+                minimizerOptions: {
+                    preset: [
+                        'default',
+                        {
+                            discardComments: { removeAll: true },
+                        },
+                    ],
+                },
+            })
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader' },
+                    {
+                        // Run postcss actions
+                        loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: function () {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+                    },
+                    { loader: 'sass-loader' },
+                ]
+            }
+        ]
+    },
     devServer: {
         contentBase: [
             path.resolve(__dirname, 'build')
